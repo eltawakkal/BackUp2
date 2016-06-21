48fde9257d3690e9fc18a66066d6783b8964f315
eltawakkal <fadhli.tech@gmail.com>
2016-06-22 00:53:06 +0700

    backup6

--- app/src/main/java/com/example/eltawakkal/myapplication/MainActivity.java
+++ app/src/main/java/com/example/eltawakkal/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.eltawakkal.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,13 +11,17 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.eltawakkal.myapplication.backup6.ListView_Class;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mButton;
     private TextView mText;
     private String alamat;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
         mButton = (Button)findViewById(R.id.btChange);
         mText = (TextView)findViewById(R.id.txAlamat);
+        imageView = (ImageView)findViewById(R.id.imgLogo);
+
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mText.setText(getAlamat());
+            }
+        });
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListView_Class.class);
+                startActivity(intent);
             }
         });
 
